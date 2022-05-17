da5477112de90941110cd2cd697b132369779b40
Tom Lemelle <tom@web-citizens.com>
2022-05-17 10:48:26 +0200

    TOUT EST GOOD

--- src/app/mock/InMemoryDataService.ts
+++ src/app/mock/InMemoryDataService.ts
@@ -5,8 +5,12 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
   const movies: Movie[] = [
     {
-      title: 'test',
+      title: 'Voyage au centre de la terre 2',
       img: 'https://resize1.prod.docfr.doc-media.fr/s/1200/img/var/doctissimo/storage/images/fr/www/famille/activites-enfants/meilleurs-films-netflix-pour-enfants/voyage-centre-terre-2-ile-mysterieuse/8176973-1-fre-FR/Voyage-au-centre-de-la-terre-2-l-ile-mysterieuse.jpg',
+    },
+    {
+      title: 'Bright',
+      img: 'https://cinedweller.com/wp-content/uploads/2020/02/bright-david-ayer-affiche-netflix.jpg',
     }
   ];
   return { movies };
