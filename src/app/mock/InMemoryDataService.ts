a3a961e69376dd80830825d5bcf32a3d993e0875
Tom Lemelle <tom@web-citizens.com>
2022-05-17 10:45:52 +0200

    fix image import

--- src/app/mock/InMemoryDataService.ts
+++ src/app/mock/InMemoryDataService.ts
@@ -1,11 +1,12 @@
 import { InMemoryDbService} from "angular-in-memory-web-api";
+import {Movie} from "../models/movie";
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
   const movies: Movie[] = [
     {
       title: 'test',
-      img: '',
+      img: 'https://resize1.prod.docfr.doc-media.fr/s/1200/img/var/doctissimo/storage/images/fr/www/famille/activites-enfants/meilleurs-films-netflix-pour-enfants/voyage-centre-terre-2-ile-mysterieuse/8176973-1-fre-FR/Voyage-au-centre-de-la-terre-2-l-ile-mysterieuse.jpg',
     }
   ];
   return { movies };
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MoviesService} from "../../services/movies.service";
+import {Movie} from "../../models/movie";
 
 @Component({
   selector: 'app-home',
