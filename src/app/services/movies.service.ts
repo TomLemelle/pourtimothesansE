a688a35a1425a9deaa56b1d51f35c029879821cb
Tom Lemelle <tom@web-citizens.com>
2022-05-17 10:20:23 +0200

    service & package

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private _client: HttpClient) { }
+
+  GetMovies(): Observable<any> {
+    return this._client.get('api/movies');
+  }
+
+  getMovie(title: string) {
+
+  }
+}
